354294017caccda7be5cfbead4715dc7785284d7
sudan <sudan@douban.com>
2016-07-08 15:52:13 +0800

    merge 2 slides

--- template/make_slides.py
+++ template/make_slides.py
@@ -9,9 +9,6 @@ IMPORT_RE = re.compile(r'^__IMPORT_(?P<filename>.*)__$')
 
 def main():
     for line in sys.stdin:
-        # js, hosted = 'remark-0.4.6.min.js', 'http://gnab.github.com/remark/downloads/'
-        # if js in line:
-        #     line = line.replace(js, hosted + js)
         m = IMPORT_RE.match(line.strip())
         if not m:
             sys.stdout.write(line)
